ab475c70ecaf3c6c9d2da0136af5d18b347f47d5
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2014-08-11 10:18:50 +0900

    implement private member

--- js/main.js
+++ js/main.js
@@ -18,58 +18,60 @@ window.onload = function() {
     core.start();
 }
 
-var Field = enchant.Class.create(enchant.Group, {
-  initialize : function() {
-    this.core = enchant.Core.instance;
-    enchant.Group.call(this, this.core.width, this.core.height);
+var Field = enchant.Class.create(enchant.Group, (function() {
+  var core, background, ground, koguma;
+  var enemies = [];
 
-    var background = new Sprite(this.core.width, this.core.height);
+  function initialize() {
+    core = enchant.Core.instance;
+    enchant.Group.call(this, core.width, core.height);
+
+    background = new Sprite(core.width, core.height);
+    ground = new Ground();
     this.addChild(background);
+    this.addChild(ground);
 
-    this.ground = new Ground();
-    this.addChild(this.ground);
+    koguma = new Koguma(50, 75, core.height - ground.height - 75);
+    koguma.x = 0;
+    koguma.y = core.height - ground.height - koguma.height;
 
-    this.koguma = new Koguma(50, 75, this.core.height - this.ground.height - 75);
-    this.koguma.x = 0;
-    this.koguma.y = this.core.height - this.ground.height - this.koguma.height;
-
-    this.addChild(this.koguma);
+    this.addChild(koguma);
 
     this.addEventListener(Event.ENTER_FRAME, function() {
-      this.enterFrame();
+      enterFrame.call(this);
     });
 
     this.addEventListener(Event.TOUCH_END, function() {
-      this.koguma.jump();
+      koguma.jump();
     });
+  };
 
-    this.enemies = [];
-  },
-  enterFrame : function() {
+  function enterFrame() {
     if (rand(50) == 1) {
-      var enemy = this.createEnemy();
-      this.enemies.push(enemy);
+      var enemy = createEnemy.call(this);
+      enemies.push(enemy);
       this.addChild(enemy);
     }
 
-    for (var i = 0; i < this.enemies.length; i++) {
-      if (this.koguma.intersect(this.enemies[i])) {
-        this.core.end();
+    for (var i = 0; i < enemies.length; i++) {
+      if (koguma.intersect(enemies[i])) {
+        core.end();
       }
     }
-  },
-  createEnemy : function() {
+  };
+
+  function createEnemy() {
     var enemy = new Enemy();
-    enemy.x = this.core.width;
-    enemy.y = this.core.height - this.ground.height - enemy.height;
+    enemy.x = core.width;
+    enemy.y = core.height - ground.height - enemy.height;
 
     var self = this;
     enemy.addEventListener('dissapear_enemy', function(event) {
-      self.removeChild(this);
+      self.removeChild(enemy);
 
-      for (var i = 0; i < self.enemies.length; i++) {
-        if (self.enemies[i] == this) {
-          self.enemies.splice(i, 1);
+      for (var i = 0; i < enemies.length; i++) {
+        if (enemies[i] == enemy) {
+          enemies.splice(i, 1);
           break;
         }
       }
@@ -77,94 +79,124 @@ var Field = enchant.Class.create(enchant.Group, {
 
     return enemy;
   }
-});
 
-var Koguma = enchant.Class.create(enchant.Sprite, {
-  initialize : function(width, height, defaultPositionY) {
+  return {
+    initialize : initialize
+  };
+})());
+
+var Koguma = enchant.Class.create(enchant.Sprite, (function(){
+
+  var core, images, frameCount, isJumping, jumpSpeed, defaultPositionY;
+
+  function initialize(width, height, y) {
     enchant.Sprite.call(this, width, height);
-    this.defaultPositionY = defaultPositionY;
+    defaultPositionY = y;
 
-    this.core = enchant.Core.instance;
-    this.images = [
-      this.core.assets['./koguma1.png'],
-      this.core.assets['./koguma2.png']
+    core = enchant.Core.instance;
+    images = [
+      core.assets['./koguma1.png'],
+      core.assets['./koguma2.png']
     ];
-    this.frameCount = 0;
-    this.isJumping = false;
+    frameCount = 0;
+    isJumping = false;
 
     this.addEventListener(Event.ENTER_FRAME, function() {
-      if (this.isJumping) {
-        this.calcPosition();
+      if (isJumping) {
+        calcPosition.call(this);
       }
-      else if (this.frameCount++ % 10 == 0) {
-        this.walk();
+      else if (frameCount++ % 10 == 0) {
+        walk.call(this);
       }
     });
-  },
-  walk : function() {
-    if (this.image == this.images[0]) {
-      this.image = this.images[1];
+  };
+
+  function walk() {
+    if (this.image == images[0]) {
+      this.image = images[1];
     }
     else {
-      this.image = this.images[0];
+      this.image = images[0];
     }
-  },
-  jump : function() {
-    if (!this.isJumping) {
-      this.jumpSpeed = 15;
-      this.isJumping = true;
+  };
+
+  function jump() {
+    if (!isJumping) {
+      jumpSpeed = 15;
+      isJumping = true;
     }
-  },
-  calcPosition : function() {
-    this.jumpSpeed -= 1;
+  };
 
-    this.y = this.y - this.jumpSpeed;
-    if (this.y > this.defaultPositionY) {
-      this.y = this.defaultPositionY;
-      this.isJumping = false;
+  function calcPosition() {
+    jumpSpeed -= 1;
+
+    this.y = this.y - jumpSpeed;
+    if (this.y > defaultPositionY) {
+      this.y = defaultPositionY;
+      isJumping = false;
     }
-  }
-});
+  };
 
-var Ground = enchant.Class.create(enchant.Sprite, {
-  initialize : function() {
-    this.core = enchant.Core.instance;
-    enchant.Sprite.call(this, this.core.width + 16, 32);
+  return {
+    initialize : initialize,
+    jump : jump
+  };
+})());
 
-    this.image = this.core.assets['./ground.png'];
+var Ground = enchant.Class.create(enchant.Sprite, (function(){
+
+  var core, frameCount;
+
+  function initialize() {
+    core = enchant.Core.instance;
+    enchant.Sprite.call(this, core.width + 16, 32);
+
+    this.image = core.assets['./ground.png'];
     this.x = 0;
-    this.y = this.core.height - 32;
+    this.y = core.height - 32;
 
-    this.frameCount = 0;
+    frameCount = 0;
     this.addEventListener(Event.ENTER_FRAME, function() {
-      this.frameCount++;
-      this.move();
+      frameCount++;
+      move.call(this);
     });
-  },
-  move : function() {
-    this.x = - this.frameCount % 16;
-  }
-});
+  };
 
-var Enemy = enchant.Class.create(enchant.Sprite, {
-  initialize : function() {
+  function move() {
+    this.x = - frameCount % 16;
+  };
+
+  return {
+    initialize : initialize
+  };
+})());
+
+var Enemy = enchant.Class.create(enchant.Sprite, (function(){
+  var core, frameCount;
+
+  function initialize() {
     enchant.Sprite.call(this, 30, 44);
-    this.core = enchant.Core.instance;
+    core = enchant.Core.instance;
 
-    this.image = this.core.assets['./enemy.png'];
+    this.image = core.assets['./enemy.png'];
 
-    this.frameCount = 0;
+    frameCount = 0;
     this.addEventListener(Event.ENTER_FRAME, function() {
-      this.frameCount++;
-      this.move();
+      frameCount++;
+      move.call(this);
     });
-  },
-  move : function() {
+  };
+
+  function move() {
     this.x -= 8;
 
     if (this.x + this.width < 0) {
       var event = new enchant.Event('dissapear_enemy');
       this.dispatchEvent(event);
     }
-  }
-});
+  };
+
+  return {
+    initialize : initialize
+  };
+})());
